a972de4c77dd76f2843af99e2461bb60498facc6
H <37481947+H-QueerCoded@users.noreply.github.com>
2025-06-05 13:17:41 +1200

    Update for 1.20.11

--- NBCartographer/Systems/WorldMap/WaypointLayer/SharedWaypointMapLayer.cs
+++ NBCartographer/Systems/WorldMap/WaypointLayer/SharedWaypointMapLayer.cs
@@ -621,7 +621,7 @@ namespace NB.Cartographer
         }
 
         // Misc
-        public override void OnViewChangedServer(IServerPlayer fromPlayer, List<Vec2i> nowVisible, List<Vec2i> nowHidden)
+        public override void OnViewChangedServer(IServerPlayer fromPlayer, int x1, int z1, int x2, int z2)
         {
             ResendWaypoints(fromPlayer);
         }
